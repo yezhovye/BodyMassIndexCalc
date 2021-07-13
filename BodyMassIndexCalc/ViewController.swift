4814a1d5ebf90d3684a05c73c9f43d72b71caf7b
Sawwaty <yezhovye@mac.com>
2021-07-13 09:56:29 +0300

    Initial Commit

---
+++ BodyMassIndexCalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BodyMassIndexCalc
+//
+//  Created by Victoria Klimova on 13.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
