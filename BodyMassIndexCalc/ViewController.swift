165719e8d5f865e683d4cff91b5eac5614a1889c
Sawwaty <yezhovye@mac.com>
2021-07-13 21:51:43 +0300

    UI elements were added, outlets and action connected

--- BodyMassIndexCalc/ViewController.swift
+++ BodyMassIndexCalc/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var bmiLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        let weight = Double(weightTextField.text!)!
+        let height = Double(heightTextField.text!)!
+        
+        let bmi = weight/(height * weight)
+        print(bmi)
+        
+        bmiLabel.text = String(bmi)
+    }
 }
 
